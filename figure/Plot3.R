7c5a1cba56851dc74c9682f61a18804724e5bb2c
B Sundaresan <sundaresan14791@gmail.com>
2017-06-25 09:52:32 +0530

    Update Plot3.R

--- figure/Plot3.R
+++ figure/Plot3.R
@@ -39,6 +39,8 @@ plot(household_final$Date, household_final$Sub_metering_1, type = "l", xlab = ""
 lines(household_final$Date, household_final$Sub_metering_2, type = "l", col="red")
 
 lines(household_final$Date, household_final$Sub_metering_3, type = "l", col="blue")
+                                 
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
 
 dev.copy(png, file="Plot3.png")
 
